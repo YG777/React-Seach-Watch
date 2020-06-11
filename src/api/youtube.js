54242f07112dd99c8a39a943451a98f267855543
YG777 <katyake@gmail.com>
2020-06-11 11:49:30 +0100

    error msg

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,43 +1,26 @@
 import axios from "axios";
 
 const youtubeData = async (searchTerm) => {
+  try {
+    const youtube = axios.create({
+      baseURL: "https://www.googleapis.com/youtube/v3",
+    });
 
-  const youtube = axios.create({
-    baseURL: "https://www.googleapis.com/youtube/v3",
-  });
- 
-  const KEY = process.env.REACT_APP_UTUBE_API_KEY;
+    const KEY = process.env.REACT_APP_UTUBE_API_KEY;
 
-  const videoData = await youtube.get("/search", {
-    params: {
-      q: searchTerm,
-      part: "snippet",
-      type: "video",
-      maxResults: 5,
-      key: `${KEY}`,
-    },
-  })
-  return videoData.data.items
-}
-
-
-
-// const youtubeData = async  (searchTerm) => {
-//   const KEY = process.env.REACT_APP_UTUBE_API_KEY;
-
-//   const res = await youtube.get("/search", {
-//     params: {
-//       q: searchTerm,
-//       part: "snippet",
-//       type: "video",
-//       maxResults: 5,
-//       key: `${KEY}`,
-//     },
-//   });
-// return res.data.items;
-
-// };
-
-
+    const videoData = await youtube.get("/search", {
+      params: {
+        q: searchTerm,
+        part: "snippet",
+        type: "video",
+        maxResults: 5,
+        key: `${KEY}`,
+      },
+    });
+    return videoData.data.items;
+  } catch (err) {
+      return err.message;
+  } 
+};
 
 export default youtubeData;
